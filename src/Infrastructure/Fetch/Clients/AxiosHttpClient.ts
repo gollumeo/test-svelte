10c9e0fd69a5ca3dfbb272302979ac0352fb5ab5
Golluméo <p.mauriello54@gmail.com>
2025-06-25 11:34:22 +0200

    fix(fetch): enhance error handling in `AxiosHttpClient`
    Updated the `AxiosHttpClient` `get` method to include error handling for failed HTTP requests. The method now throws a descriptive error with the HTTP status code and reason, improving diagnostic capabilities and robustness.

--- src/Infrastructure/Fetch/Clients/AxiosHttpClient.ts
+++ src/Infrastructure/Fetch/Clients/AxiosHttpClient.ts
@@ -1,11 +1,23 @@
 import type { IFetchClient } from "@infrastructure/Fetch/Contracts/IFetchClient";
-import axios, { type AxiosResponse } from "axios";
+import axios, { type AxiosError, type AxiosResponse } from "axios";
 
 export class AxiosHttpClient implements IFetchClient
 {
     async get(url: string): Promise<object[]>
     {
-        const result: AxiosResponse = await axios.get(url);
-        return result.data;
+        try 
+        {
+            const result: AxiosResponse = await axios.get(url);
+            return result.data;
+        }
+        catch (error: unknown) 
+        {
+            const axiosError = error as AxiosError;
+
+            const status = axiosError.response?.status ?? 'unknown';
+            const reason = axiosError.response?.statusText ?? axiosError.message ?? 'unknown error';
+
+            throw new Error(`Axios Error: ${ status } — ${ reason }`)
+        }
     }
 }
